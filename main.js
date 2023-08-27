bf4e82ef9a27e60fa66a9431e0bb3d2f21fc3f47
fer-tacosdecaca <130688038+fer-tacosdecaca@users.noreply.github.com>
2023-08-27 11:15:12 -0600

    main.js

--- main.js
+++ main.js
@@ -9,4 +9,38 @@ Webcam.set({
     png_quality:90
   });
 
+  
+
+  function pipi(){
+    Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_uri+'"/>';
+    })
+  } 
+
+
+
+
+
+
+
+
+
+
+
+  console.log("ml5 version:",ml5.version)
+
+
   classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Y8SVGIw2K/model.json',modelLoaded);
+
+ function modelLoaded(){
+console.log("ta bien :)")
+  }
+
+function caca(){
+  img = document.getElementById('selfie_image');
+  classifier.classify(img,gotresult);
+}
+
+function gotresult(results){
+console.log(results)
+}
